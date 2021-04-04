46d8ee237d0a52abf6cf04c0a6daae26b342da53
Harrison Noh <nohharri@github.com>
2021-04-04 09:04:03 -0400

    Added animation

--- src/components/SocialMediaLink/SocialMediaLink.js
+++ src/components/SocialMediaLink/SocialMediaLink.js
@@ -3,13 +3,37 @@ import styles from './SocialMediaLink.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default class SocialMediaLink extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { counter: 0 };
+    }
+
+    componentDidMount() {
+        this.startTimer();
+    }
+
+    startTimer() {
+        const { counter } = this.state;
+        const { followers } = this.props;
+
+        setTimeout(() => {
+            if (counter < followers) {
+                this.setState({ counter: counter + 1 });
+                this.startTimer();
+            }
+        }, 1);
+        
+    }
+
     render() {
-        const { icon, followers, url } = this.props;
+        const { counter } = this.state;
+        const { icon, url } = this.props;
         return (
             <div className={styles.container}>
                 <a href={url}>
                     <FontAwesomeIcon className={styles.icon} icon={icon} />
-                    <div className={styles.text}>{followers}</div>
+                    <div className={styles.text}>{counter}</div>
                 </a>
             </div>
         )
